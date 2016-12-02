3310dbd55478415c3ae7f20a63b67822ca6fc45b
Manuel Lopez <jmlopez@iofficecorp.com>
2016-12-01 18:13:38 -0600

    [chore] updating task file
    Using a class with static members instead of an object.

--- src/tasks.ts
+++ src/tasks.ts
@@ -2,27 +2,29 @@ import { updateReadme } from './readme';
 import { testGuide, formatResults } from './test';
 import { writeNewRule, writeNewRuleTests } from './util';
 
-
-const tasks: any = {
-  readme() {
+class Task {
+  /**
+   * Generate the readme file from the information in `src/guide`.
+   */
+  static readme() {
     updateReadme();
-  },
+  }
 
   /**
    * Runs all the tests written in the guide to make sure that all the expected errors are being
    * reported by tslint.
    */
-  testGuide(topic?: string) {
+  static testGuide(topic?: string) {
     const results = testGuide(topic);
     process.stdout.write(formatResults(results));
-  },
+  }
 
   /**
    * Creates template files in `src/rules` and `src/test/rules`.
    *
    * @param ruleName Must be in kebab-case
    */
-  newRule(ruleName: string) {
+  static newRule(ruleName: string) {
     if (!ruleName) {
       console.error('ERR: provide the rule name');
       return;
@@ -30,7 +32,7 @@ const tasks: any = {
     writeNewRule(ruleName);
     writeNewRuleTests(ruleName);
   }
-};
+}
 
-const task = process.argv[2];
-tasks[task](...process.argv.slice(3));
+const taskName: string = process.argv[2];
+Task[taskName](...process.argv.slice(3));
