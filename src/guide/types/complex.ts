7fab9f87f0c67295986b7b08aa7bbf40a6e59704
Manuel Lopez <jmlopez@iofficecorp.com>
2018-04-26 20:39:46 -0500

    [guide] added types topic

---
+++ src/guide/types/complex.ts
@@ -0,0 +1,33 @@
+import { ISection, dedent, expecting } from '../../util';
+
+const section: ISection = {
+  name: 'Complex',
+  reference: 'complex',
+  rule: dedent`
+    When you access a complex type you work on a reference to its value.
+    
+    - \`object\`
+    - \`array\`
+    - \`function\`
+    `,
+  tslint: {
+  },
+  examples: [
+    {
+      code: dedent`
+        const foo: number[] = [1, 2];
+        const bar: number[] = foo;
+        
+        bar[0] = 9;
+        
+        console.log(foo[0], bar[0]); // => 9, 9
+        `,
+      errors: expecting([
+      ]),
+    },
+  ],
+};
+
+export {
+  section,
+};
---
+++ src/guide/types/primitives.ts
@@ -0,0 +1,57 @@
+import { ISection, dedent, expecting } from '../../util';
+
+const section: ISection = {
+  name: 'Primitives',
+  reference: 'primitives',
+  rule: dedent`
+    When you access a primitive type you work directly on its value.
+    
+    - \`number\`
+    - \`string\`
+    - \`boolean\`
+    - \`null\`
+    - \`undefined\`
+    
+    These types can be inferred by the typescript compiler and should not explicitly typed.
+    `,
+  reason: dedent`
+    Why? Explicit types where they can be easily inferred by the compiler make code more verbose.
+    `,
+  tslint: {
+    'no-inferrable-types': 'https://palantir.github.io/tslint/rules/no-inferrable-types/',
+  },
+  examples: [
+    {
+      code: dedent`
+        const foo = 1;
+        let bar = foo;
+        
+        bar = 9;
+        
+        console.log(foo, bar);  // => 1, 9
+        `,
+      errors: expecting([
+      ]),
+    },
+    {
+      code: dedent`
+        // bad
+        const foo: number = 1;
+        
+        // good
+        let bar: number = foo;
+        
+        bar = 9;
+        
+        console.log(foo, bar);  // => 1, 9
+        `,
+      errors: expecting([
+        [2, 11, 'no-inferrable-types', 'Type number trivially inferred from a number literal, remove type annotation']
+      ]),
+    },
+  ],
+};
+
+export {
+  section,
+};
