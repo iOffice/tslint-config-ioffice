6b17cf3d2aca093421c9860643791513e7dac8a6
Manuel Lopez <jmlopez@iofficecorp.com>
2018-11-14 14:37:06 -0600

    [feat] forcing one type assertion style

--- src/guide/types/index.ts
+++ src/guide/types/index.ts
@@ -1,11 +1,13 @@
 import * as complex from './complex';
 import * as primitives from './primitives';
+import * as typeAssertion from './type-assertion';
 
 const name = 'Types';
 const reference = 'types';
 const order: any[] = [
   primitives,
   complex,
+  typeAssertion,
 ];
 
 export {
